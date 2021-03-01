650fb855d23ce52957b64fe4003d815bef9ecff4
Zachary Nathoo <zachary.nack@icloud.com>
2021-02-28 21:10:11 -0700

    remove gnu tools from path to stop breaking homebrew

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
 
 # 	ENV
-set PATH $HOME/.local/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/coreutils/libexec/gnubin $PATH
-export GOPATH="$HOME/Code/go"
 
 
 #	ALIASES
-alias la="ls -a"
+alias ls="gls --color=auto"
+alias la="gls -a"
+alias ll="gls -la"
 alias altchrome="open -n -a 'Google Chrome' --args --user-data-dir=$HOME/.local/cache/chrome"
 alias py="python3"
